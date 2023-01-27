47655536a79fb6c35f336f10ef694e273f08ef77
Bryan <31880091+raizdev@users.noreply.github.com>
2023-01-27 02:55:54 +0100

    Create Dashboard.vue

---
+++ src/views/admin/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+       <UserList 
+          v-if="!this.contractorRecipients" 
+       />
+    </div>
+ </template>
+ <script>
+ import { mapActions, mapState } from 'pinia'
+ import { useUserStore } from '@/stores'
+ 
+ import UserList from '@/components/Module/Admin/UserList.vue';
+    
+ export default {
+    data() {
+       return {
+       }
+    },
+ 
+    components: {
+       UserList
+    },
+ 
+    computed: {
+       ...mapState(
+          useUserStore, { 
+             pendingUsers: 'pendingUsers'
+          }
+       )
+    },
+ 
+    methods: {
+       ...mapActions(
+          useUserStore, { 
+            userList: 'userList',
+          }
+       )
+    },
+ 
+    mounted() {
+       this.getContractors()
+    }
+ }
+ </script>
