bda5e4624d31b3723a91c9484771ce33ec272014
Bryan <31880091+raizdev@users.noreply.github.com>
2023-01-27 01:55:00 +0100

    Delete src/Router directory

--- src/Router/account.routes.js
+++
@@ -1,9 +0,0 @@
-import { Signin, Signup } from '@/views/account';
-
-export default {
-    path: '/account',
-    children: [
-        { path: 'login', name: 'sign-in', component: Signin },
-        { path: 'register', name: 'sign-up', component: Signup }
-    ]
-};
