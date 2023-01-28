d1578133b76dc478fb15d446c26725eadeb3c770
starr500 <31880091+bvdstarre@users.noreply.github.com>
2023-01-28 15:08:48 +0100

    [TASK] started admin UserEdit

---
+++ src/stores/users.store.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import api from "@/includes/api";
+
+export const useUsersStore = defineStore({
+    id: 'users',
+    state: () => ({
+        users: null
+    }),
+    getters: {
+        activatedUsers: (state) => (activated) => {
+            return state.users.filter(users => users.activated == activated)
+        }
+    },
+    actions: {
+        async userList() {
+            const response = await api.get('user/list')
+            this.users = response
+        },
+
+        async modifyPendingUser(user, type) {
+            const response = await api.get('user/modifyPendingUser', { user: user.id, type: type })
+            return response
+        },
+    }
+});
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,44 +1,25 @@
 <template>
     <div>
        <UserList 
-          v-if="!this.contractorRecipients" 
+          :users="this.userList" 
        />
     </div>
  </template>
  <script>
  import { mapActions, mapState } from 'pinia'
- import { useUserStore } from '@/stores'
+ import { useUsersStore } from '@/stores'
  
  import UserList from '@/components/Module/Admin/UserList.vue';
     
  export default {
-    data() {
-       return {
-       }
-    },
- 
-    components: {
-       UserList
-    },
- 
-    computed: {
-       ...mapState(
-          useUserStore, { 
-             pendingUsers: 'pendingUsers'
-          }
-       )
-    },
- 
-    methods: {
-       ...mapActions(
-          useUserStore, { 
-            userList: 'userList',
-          }
-       )
-    },
- 
-    mounted() {
-       this.getContractors()
-    }
+   data() {
+      return {
+         userList: []
+      }
+   },
+
+   components: {
+      UserList
+   }
  }
  </script>
