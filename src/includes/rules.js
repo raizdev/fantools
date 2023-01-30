ace443bda8543f8c4e30a4d4c30e9208479828bd
bryan <raiz@live.nl>
2023-01-30 01:26:04 +0100

    [TASK] only domain mail is allowed while registereing

--- src/includes/rules.js
+++ src/includes/rules.js
@@ -1,13 +1,7 @@
-import { defineRule  } from "vee-validate";
-import store from "../../../Store"
-
-
-export const usernameAvailable = (value) => {
-    if (!value) {
+export const emailContainsDomain = (value) => {
+    if (value.endsWith('@kpn.com')) {
         return true;
     }
 
-    return store.dispatch('auth/userAvailability', {username: value }).then((res) => {
-        return res.availability === 0
-    });
+    return false;
 }
